d82ba23a4fc3bd093d5d132dd81dd254ff681529
Takashi Yoshida <takashi@takashiyoshida.org>
2014-06-15 20:45:09 +0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class flip4mac (
+  $version = '3.3.1'
+) {
+  package { 'Flip4Mac':
+    ensure => installed,
+    provider => 'appdmg',
+    source   => "http://www.telestream.net/download-files/flip4mac/3-3/Flip4Mac-${version}.dmg"
+  }
 }
