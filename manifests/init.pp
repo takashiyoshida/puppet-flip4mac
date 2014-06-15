ef7a8b6419aa981cd09484dcdff749a36de59738
Takashi Yoshida <takashi@takashiyoshida.org>
2014-06-15 20:52:22 +0800

    Fix incorrect indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class flip4mac (
   $version = '3.3.1'
 ) {
   package { 'Flip4Mac':
-    ensure => installed,
+    ensure   => installed,
     provider => 'appdmg',
     source   => "http://www.telestream.net/download-files/flip4mac/3-3/Flip4Mac-${version}.dmg"
   }
